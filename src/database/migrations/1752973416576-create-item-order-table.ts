4a4bf85676c7c13040e1e618115fd635ca7bc802
HeitorFonseca-Dev <heitorr.fonseca@gmail.com>
2025-07-19 22:21:34 -0300

    feat: criacao da entidade de itens do pedido

---
+++ src/database/migrations/1752973416576-create-item-order-table.ts
@@ -0,0 +1,97 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateItemOrderTable1752973416576 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'order_items',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'order_id',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'product_id',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'quantity',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'unit_price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+            isNullable: false,
+          },
+          {
+            name: 'subtotal',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'created_by',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_by',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+          {
+            name: 'deleted_by',
+            type: 'varchar',
+            isNullable: true,
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ['order_id'],
+            referencedTableName: 'order',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+          },
+          {
+            columnNames: ['product_id'],
+            referencedTableName: 'product',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('order_items');
+  }
+}
